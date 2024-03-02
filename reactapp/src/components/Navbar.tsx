6b842eeed92d9fb175041ef00f13e5d6f0277ab2
michellevit <mvflandin@gmail.com>
2024-03-01 21:05:44 -0800

    Add hamburger nav

--- reactapp/src/components/Navbar.tsx
+++ reactapp/src/components/Navbar.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav>
       <Link to="/" className="title">DG Bets</Link>
-      <ul>
+      <div className={"menu" + (menuOpen ? " icon-open" : "")} onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={menuOpen ? "open" : ""}>
         <li><NavLink to="/bets">Bets</NavLink></li>
         <li><NavLink to="/profile">Profile</NavLink></li>
       </ul>
