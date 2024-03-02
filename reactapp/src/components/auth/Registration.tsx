31f23f25b2504276ed2c349d54b4d0f53e42a347
michellevit <mvflandin@gmail.com>
2024-03-02 14:23:40 -0800

    Add form axios request

--- reactapp/src/components/auth/Registration.tsx
+++ reactapp/src/components/auth/Registration.tsx
@@ -1,5 +1,6 @@
 import React, { useState, FormEvent } from "react";
-
+import axios from "axios";
+import "./Registration.css";
 
 const Registration: React.FC = () => {
   const [registered, setRegistered] = useState(false);
@@ -7,13 +8,42 @@ const Registration: React.FC = () => {
   const [password, setPassword] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
   const [registrationErrors, setRegistrationErrors] = useState("");
+  const [successfulSubmission, setSuccessfulSubmission] = useState<boolean>(false);
+  const [confirmationMessage, setConfirmationMessage] = useState("");
 
+
+  
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    axios.post(`${process.env.REACT_APP_API_URL}/registrations`, {
+      user: {
+        email: email,
+        password: password,
+        password_confirmation: passwordConfirmation
+      }
+    },
+    { withCredentials: true }
+    ).then(response => {
+      console.log("registration res", response);
+    }).catch(error => {
+      console.log("registration error", error);
+    })
+
     console.log("Email: ", email);
     console.log("Password: ", password);
     console.log("Password Confirmation: ", passwordConfirmation);
     console.log('Form Submitted');
+    if (password === passwordConfirmation) {
+      setEmail("");
+      setPassword("");
+      setPasswordConfirmation("");
+      setSuccessfulSubmission(true);
+      setConfirmationMessage("You have been registered successfully.")
+    }
+    else {
+      setSuccessfulSubmission(false);
+      setConfirmationMessage("The passwords do not match.")
+    }
   }
 
   return (
@@ -48,6 +78,7 @@ const Registration: React.FC = () => {
         </div>
         <button type="submit">Register</button>
     </form>
+    {confirmationMessage && <div className={successfulSubmission ? "success" : "fail"}>{confirmationMessage}</div>}
   </div>
   )
 }
