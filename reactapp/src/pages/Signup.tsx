8475a225c8e47e08def6ba44b10908e4e331e1ad
michellevit <mvflandin@gmail.com>
2024-03-02 15:27:22 -0800

    Rearrange pages to be more clear

--- reactapp/src/pages/Signup.tsx
+++ reactapp/src/pages/Signup.tsx
@@ -3,9 +3,9 @@ import Registration from "../components/auth/Registration";
 import { LoggedInProps } from "../types/interfaces";
 
 
-const Signup: React.FC<LoggedInProps> = ({ loggedIn }) => {
+const Signup: React.FC<LoggedInProps> = ({ loggedIn, setLoggedIn }) => {
   return <div className="signup-container">
-    <Registration loggedIn={loggedIn}/>
+    <Registration loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
   </div>;
 }
 
