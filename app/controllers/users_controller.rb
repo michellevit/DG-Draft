ece00ccd10eab891241568f1bdaac6cc206b64dd
michellevit <mvflandin@gmail.com>
2024-03-03 17:32:47 -0800

    Modify token authenticator before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,23 @@
 class UsersController < ApplicationController
   include Tokenable
-  before_action :authenticate_token, only: [:update_username]
   
   def update_username
-    requested_username = params[:username]
-    if @current_user.username == requested_username
-      render json: { error: 'New username cannot be the same as the current one.' }, status: :unprocessable_entity
-    elsif User.exists?(username: requested_username)
-      render json: { error: 'Username already in use' }, status: :unprocessable_entity
-    elsif @current_user.update(username: requested_username)
-      render json: { user: @current_user }
+    token = request.headers['Authorization']&.split(' ')&.last
+    @current_user = authenticate_token(token) if token
+    if @current_user
+      requested_username = params[:username]
+      if @current_user.username == requested_username
+        render json: { error: 'New username cannot be the same as the current one.' }, status: :unprocessable_entity
+      elsif User.exists?(username: requested_username)
+        render json: { error: 'Username already in use' }, status: :unprocessable_entity
+      elsif @current_user.update(username: requested_username)
+        render json: { user: @current_user }
+      else
+        render json: { error: 'Failed to update username' }, status: :unprocessable_entity
+      end
     else
-      render json: { error: 'Failed to update username' }, status: :unprocessable_entity
+      render json: { error: 'Invalid token' }, status: :unauthorized
     end
   end
 
 end
-  
