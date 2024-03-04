39acdc9b5b031e3b87d29ab6deaa37f74d281153
michellevit <mvflandin@gmail.com>
2024-03-03 19:41:10 -0800

    Add puts log statements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,17 +7,24 @@ class UsersController < ApplicationController
     @current_user = authenticate_token(token) if token
     puts "Authenticated user: #{@current_user.inspect}" 
     if @current_user
+      puts "GOT HERE"
       requested_username = params[:username]
+      puts "A"
       if @current_user.username == requested_username
+        puts "B"
         render json: { error: 'New username cannot be the same as the current one.' }, status: :unprocessable_entity
       elsif User.exists?(username: requested_username)
+        puts "C"
         render json: { error: 'Username already in use' }, status: :unprocessable_entity
       elsif @current_user.update(username: requested_username)
+        puts "D"
         render json: { user: @current_user }
       else
+        puts "E"
         render json: { error: 'Failed to update username' }, status: :unprocessable_entity
       end
     else
+      puts "F"
       render json: { error: 'Invalid token' }, status: :unauthorized
     end
   end
