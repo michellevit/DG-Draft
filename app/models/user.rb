7842cc508d5cede6e7e0c15b4b9f965abf6ff906
michellevit <mvflandin@gmail.com>
2024-03-03 20:34:55 -0800

    Modify username max lenth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
     validates_presence_of :email
     validates_uniqueness_of :email
     validates :password, length: { minimum: 6 }
-    validates :username, presence: true, uniqueness: true, length: { maximum: 25 }
-end
+    validates :username, presence: true, length: { maximum: 25 }, uniqueness: { case_sensitive: false }
+  end
+  
