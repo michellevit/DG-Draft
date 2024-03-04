20110364649ce23476c20b00958d128b1842ec69
michellevit <mvflandin@gmail.com>
2024-03-03 19:01:17 -0800

    Modify routes and controller names

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   post '/sessions', to: 'sessions#attempt_login'
-  post '/sessions', to: 'sessions#create_user'
   delete :logout, to: "sessions#logout"
   get :authenticate_user, to: "sessions#authenticate_user"
-  root 'application#react_app'
+  post '/registrations', to: 'registrations#create_user'
   resources :users do
     patch 'update_username', on: :member
   end
+  root 'application#react_app'
   get "up" => "rails/health#show", as: :rails_health_check
   get '*path', to: 'application#react_app'
 end
