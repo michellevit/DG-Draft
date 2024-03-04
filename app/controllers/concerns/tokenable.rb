9aa59cc7025dbc1f2bdbb06163c4d04d86875092
michellevit <mvflandin@gmail.com>
2024-03-03 18:32:27 -0800

    Add console logs for debugging

--- app/controllers/concerns/tokenable.rb
+++ app/controllers/concerns/tokenable.rb
@@ -9,11 +9,13 @@ module Tokenable
   end
 
   def authenticate_token(token)
+    puts "Received token: #{token}"
     hmac_secret = ENV['JWT_SECRET_KEY']
     begin
       decoded_token = JWT.decode(token, hmac_secret, true, { algorithm: 'HS256' })
       user_id = decoded_token[0]['user_id'] 
       @current_user = User.find_by(id: user_id)
+      puts "Return: #{@current_user}"
       return @current_user if user_id.present? && @current_user
     rescue JWT::DecodeError, JWT::ExpiredSignature
       nil
