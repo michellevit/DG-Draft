b830f73fdd04f419133022e6e8930ce2501d18cf
michellevit <mvflandin@gmail.com>
2024-03-05 21:00:41 -0800

    Add admin status to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_06_045414) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_06_045908) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_06_045414) do
     t.datetime "updated_at", null: false
     t.string "username", limit: 25
     t.integer "points", default: 0
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
