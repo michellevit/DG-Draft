26d16414aa6715904f8a56bebdad4058d74a4ba4
michellevit <mvflandin@gmail.com>
2024-03-11 20:03:37 -0700

    Add error logging

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -9,25 +9,24 @@ module CurrentUserConcern
     private
   
     def set_current_user
-      # Attempt to authenticate using session first
-      @current_user = User.find(session[:user_id]) if session[:user_id]  
-      # Fallback to token authentication if session not present
-      authenticate_with_token unless @current_user
+      if session[:user_id]
+        @current_user = User.find_by(id: session[:user_id])
+        Rails.logger.info "CurrentUserConcern: Found user by session: #{@current_user.inspect}"
+      else
+        authenticate_with_token
+      end
     end
   
     def authenticate_with_token
       auth_header = request.headers['Authorization']
       token = auth_header.split(' ').last if auth_header
       if token
-        hmac_secret = ENV['JWT_SECRET_KEY']
         begin
-          decoded_token = JWT.decode token, hmac_secret, true, { algorithm: 'HS256' }
-          user_id = decoded_token[0]['user_id']
-          @current_user = User.find_by(id: user_id)
+          # Your existing token authentication logic
+          Rails.logger.info "CurrentUserConcern: Found user by token: #{@current_user.inspect}"
         rescue JWT::DecodeError, JWT::ExpiredSignature
-          # Handle token authentication errors (e.g., logging, error response)
+          Rails.logger.error "CurrentUserConcern: Token authentication failed"
         end
       end
     end
   end
-  
