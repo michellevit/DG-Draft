d3d40f73852a7d7bb35290de7191c865dcfe4ba4
michellevit <mvflandin@gmail.com>
2024-03-11 21:55:08 -0700

    Add console log for debugging

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -17,9 +17,12 @@ module Api
       user = User.find_by(username: params[:username])
       if user
         Rails.logger.error("Use found with username: #{params[:username]}")
+        Rails.logger.info("Use found with username: #{params[:username]}")
+
         render json: { exists: true, id: user.id }
       else
         Rails.logger.error("User not found with username: #{params[:username]}")
+        Rails.logger.info("User not found with username: #{params[:username]}")
         render json: { exists: false }, status: :not_found
       end
     end
