e1b5528248d203c2f83c1fd3a2cd518ece87f056
michellevit <mvflandin@gmail.com>
2024-03-11 20:11:35 -0700

    Revise replicated code

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -1,32 +1,35 @@
 # app/controllers/concerns/current_user_concern.rb
 module CurrentUserConcern
-    extend ActiveSupport::Concern
-    
-    included do
-      before_action :set_current_user
-    end
+  extend ActiveSupport::Concern
+  include TokenableConcern # Ensure this concern is included
   
-    private
+  included do
+    before_action :set_current_user
+  end
   
-    def set_current_user
-      if session[:user_id]
-        @current_user = User.find_by(id: session[:user_id])
-        Rails.logger.info "CurrentUserConcern: Found user by session: #{@current_user.inspect}"
-      else
-        authenticate_with_token
-      end
-    end
+  private
   
-    def authenticate_with_token
-      auth_header = request.headers['Authorization']
-      token = auth_header.split(' ').last if auth_header
-      if token
-        begin
-          # Your existing token authentication logic
-          Rails.logger.info "CurrentUserConcern: Found user by token: #{@current_user.inspect}"
-        rescue JWT::DecodeError, JWT::ExpiredSignature
-          Rails.logger.error "CurrentUserConcern: Token authentication failed"
-        end
-      end
+  def set_current_user
+    if session[:user_id]
+      @current_user = User.find_by(id: session[:user_id])
+      Rails.logger.info "CurrentUserConcern: Found user by session: #{@current_user.inspect}"
+    else
+      authenticate_with_token
     end
   end
+
+  def authenticate_with_token
+    auth_header = request.headers['Authorization']
+    token = auth_header.split(' ').last if auth_header
+    if token
+      @current_user, error_message = authenticate_token(token) # Use the method from TokenableConcern
+      if @current_user
+        Rails.logger.info "CurrentUserConcern: Found user by token: #{@current_user.inspect}"
+      else
+        Rails.logger.error "CurrentUserConcern: Token authentication failed - #{error_message}"
+      end
+    else
+      Rails.logger.info "CurrentUserConcern: No authorization token found"
+    end
+  end
+end
