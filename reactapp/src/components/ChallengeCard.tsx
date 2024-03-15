ab4bf544b97c4cfc7f4df216f45ce9e62cff54ce
michellevit <mvflandin@gmail.com>
2024-03-14 23:37:48 -0700

    Add user error catch

--- reactapp/src/components/ChallengeCard.tsx
+++ reactapp/src/components/ChallengeCard.tsx
@@ -5,11 +5,13 @@ import { Challenge, ChallengeCardProps } from '../types/interfaces';
     return (
     <div className="challenge-card">
       <h3>{challenge.event_name}</h3>
-      <p>Challenger: {challenge.challenger.username}</p>
-      <p>Challengee: {challenge.challengee.username}</p>
+      <p>Challenger: {challenge.challenger?.username}</p>
+      <p>Challengee: {challenge.challengee?.username}</p>
       <p>Date: {new Date(challenge.event_date_end).toLocaleDateString()}</p>
     </div>
   );
 };
 
 export default ChallengeCard;
+
+
