948091e4ff99f4caaccfd01511fa182f24775e10
michellevit <mvflandin@gmail.com>
2024-03-23 20:56:26 -0700

    Improve color palette

--- reactapp/src/pages/Challenges.tsx
+++ reactapp/src/pages/Challenges.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../contexts/UserContext";
 import { Link, Outlet } from "react-router-dom";
-import "./Challenge.css";
+import "./Challenges.css";
 
 
 const Challenges: React.FC = () => {
