2cfaed8bb9119baf745a1c7388c8c2c35d0b449a
michellevit <mvflandin@gmail.com>
2024-03-23 20:53:01 -0700

    Improve color palette

--- reactapp/src/components/Leaderboard.tsx
+++ reactapp/src/components/Leaderboard.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./Leaderboard.css";
 import { useUser } from "../contexts/UserContext";
 
 interface LeaderboardUser {
