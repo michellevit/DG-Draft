9b94e81feb93ef42f939bb3880726f27cd566026
michellevit <mvflandin@gmail.com>
2024-03-23 21:13:43 -0700

    Improve color palette

--- reactapp/src/components/Leaderboard.tsx
+++ reactapp/src/components/Leaderboard.tsx
@@ -32,13 +32,6 @@ const Leaderboard: React.FC = () => {
     <div className="leaderboard-container">
       <h3>Leaderboard</h3>
       <table>
-        <thead>
-          <tr>
-            <th>Placement</th>
-            <th>Username</th>
-            <th>Points</th>
-          </tr>
-        </thead>
         <tbody>
           {leaderboardData.map((leaderboardUser, index) => (
             <tr
